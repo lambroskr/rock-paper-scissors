6f14b8839f0a3c5b4f95f2798c05332456777e8c
lambroskr <54982694+lambroskr@users.noreply.github.com>
2021-08-04 16:49:49 +0300

    added a ui

--- game.js
+++ game.js
@@ -1,72 +1,87 @@
-const outcomes = ['rock','paper','scissors'];
-
-function computerPlay(){
-    return outcomes[Math.floor(Math.random() * outcomes.length )];
-}
-
 let player_score = 0;
 let computer_score = 0;
 
+
+function game(){
+
+    const buttons = document.querySelectorAll('input[type="image"]');
+
+    buttons.forEach((button) => {
+
+        button.addEventListener('click', () => {
+            computerSelection = computerPlay();
+            playerSelection = button.value;
+
+            playRound(playerSelection, computerSelection);
+            showComputerSelection(computerSelection);
+            updateScore();
+            checkWinner();
+            
+            
+        });
+    });   
+    
+}
+
+
+function computerPlay(){
+
+    const outcomes = ['rock','paper','scissors'];
+    return outcomes[Math.floor(Math.random() * outcomes.length )];
+}
+
+
 function playRound(playerSelection, computerSelection) {
 
-    
-
     if(playerSelection == computerSelection){
-        console.log("It's a tie!");
         player_score++;
         computer_score++; 
 
     }else if(playerSelection == 'rock'){
         if(computerSelection == 'scissors'){
-            console.log('Player won the Computer');
             player_score++; 
         }else{
-            console.log('Player lost to the Computer');
             computer_score++;
         }
-
     }else if(playerSelection == 'paper'){
         if(computerSelection == 'rock'){
-            console.log('Player won the Computer');
             player_score++;
-            
         }else{
-            console.log('Player lost to the Computer');
             computer_score++;
         }
-
     }else if(playerSelection == 'scissors'){
         if(computerSelection == 'paper'){
-            console.log('Player won the Computer');
             player_score++; 
         }else{
-            console.log('Player lost to the Computer');
             computer_score++;
         }
-
-    }else{
-        console.log('Error');
-    }
-}
-  
-
-
-function game(){
-    for(let i=1; i<6; i++){
-        const playerSelection = prompt().toLowerCase();
-        const computerSelection = computerPlay();
-
-        console.log("Round " + i);
-        playRound(playerSelection, computerSelection);
-
-    }
-    if(player_score > computer_score){
-        console.log('Player won the Computer with score ' + player_score + "-" + computer_score);
-    }else if(player_score < computer_score){
-        console.log('Computer won the Player with score ' + computer_score + "-" + player_score);
-    }else{
-        console.log("It's a tie! " + player_score + "-" + computer_score);
     }
 }
 
+
+function showComputerSelection(computer_selection){
+
+    const computerImg = document.getElementById('computerImg');
+    computerImg.style.display = 'block';
+    computerImg.src = computerSelection + '.png';
+}
+
+function checkWinner(){
+
+    if(player_score == 5){
+        alert('Player Won');
+        player_score = 0;
+        computer_score = 0;
+    }else if(computer_score == 5){
+        alert('Computer Won');
+        player_score = 0;
+        computer_score = 0;
+    }
+}
+
+function updateScore(){
+
+    const scoreSpan = document.getElementById('score');
+    scoreSpan.innerHTML = player_score + " - " + computer_score;
+}
 game();
